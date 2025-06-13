8529ea24fefbfa02ea6c0a8ac3bbbc701925b528
Lubomír Doležal <lubomir.dolezal@eox.at>
2025-06-13 13:48:43 +0200

    add DEF_REPO to gitpuller

--- .github/workflows/generate_notebook_list.py
+++ .github/workflows/generate_notebook_list.py
@@ -207,7 +207,7 @@ def collect_notebooks():
                     "repo": DEF_REPO,
                     "source": "local",
                     "path": rel_path,
-                    "gitpuller": f"https://{JHUB_INSTANCE}/hub/user-redirect/git-pull?repo={git_url}&urlpath=lab/tree/{rel_path}&branch=main",
+                    "gitpuller": f"https://{JHUB_INSTANCE}/hub/user-redirect/git-pull?repo={git_url}&urlpath=lab/tree/{DEF_REPO}/{rel_path}&branch=main",
                 })
 
     # --- Submodule notebooks
@@ -247,7 +247,7 @@ def collect_notebooks():
                             "repo": git_info["repo"],
                             "source": "submodule",
                             "path": rel_path,
-                            "gitpuller": f"https://{JHUB_INSTANCE}/hub/user-redirect/git-pull?repo={git_url}&urlpath=lab/tree/{repo_path}&branch=main",
+                            "gitpuller": f"https://{JHUB_INSTANCE}/hub/user-redirect/git-pull?repo={git_url}&urlpath=lab/tree/{DEF_REPO}/{repo_path}&branch=main",
                         })
 
     return catalog
